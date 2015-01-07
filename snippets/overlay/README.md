4d8adbbc7c8d009c9876502932ae9dc720584997
Julien Bouquillon <julien@bouquillon.com>
2015-01-07 02:42:24 +0100

    use window.overlay CLI for overlay

--- snippets/overlay/README.md
+++ snippets/overlay/README.md
@@ -1,3 +1,13 @@
 ### overlay.js
 
-Add an semi-transparent image overlay over your page to have pixel-perfect integration with your designs.
+`window.overlay` helper to work with semi-transparents layers for pixel-perfect designs.
+
+```js
+// create a new overlay
+var x = overlay('http://host/path/to/video-[zefz].jpg');
+
+x.toggle();
+x.rm();
+```
+
+**Note:** `file://` protocol is not supported for security reasons
--- snippets/overlay/overlay.js
+++ snippets/overlay/overlay.js
@@ -1,17 +1,52 @@
-var storageKey = 'overlay-default';
-var overlayDefault = localStorage.getItem(storageKey) || '';
-var url = prompt('paste overlay url', overlayDefault);
-if (url) {
-    localStorage.setItem(storageKey, url);
-    var overlay = document.createElement('div');
-    overlay.style.position = 'absolute';
-    overlay.style.left = 0;
-    overlay.style.top = 0;
-    overlay.style.width = '100%';
-    overlay.style.height='100%';
-    overlay.style.backgroundImage = 'url(' + url + ')';
-    overlay.style.backgroundSize = 'cover';
-    overlay.style.zIndex=10000;
-    overlay.style.opacity = 0.5;
-    document.body.appendChild(overlay);
+/*
+
+PixelPerfect overlay CLI devtools snippet
+
+usage example :
+
+// to add
+var x = overlay('http://host/path/to/video-[zefz].jpg');
+
+x.toggle();
+x.rm();
+
+*/
+
+function cleanUrl(url) {
+    var a = document.createElement('a');
+    a.href = url;
+    var encodedUrl = a.href;
+    delete a;
+    return encodedUrl;
+}
+function Overlay(url) {
+    console.log(url);
+    console.log(cleanUrl(url));
+    this.overlay = document.createElement('div');
+    this.overlay.style.display = 'block';
+    this.overlay.style.position = 'absolute';
+    this.overlay.style.left = 0;
+    this.overlay.style.top = 0;
+    this.overlay.style.width = '100%';
+    this.overlay.style.height='100%';
+    this.overlay.style.backgroundImage = 'url(' + cleanUrl(url) + ')';
+    this.overlay.style.backgroundSize = 'cover';
+    this.overlay.style.zIndex=10000;
+    this.overlay.style.opacity = 0.5;
+    document.body.appendChild(this.overlay);
+
+    this.toggle = function() {
+        this.overlay.style.display = (this.overlay.style.display==='block')?'none':'block';
+    }
+
+    this.rm = function() {
+        console.log('rm');
+        document.body.removeChild(this.overlay);
+    }
+}
+
+
+window.overlay = function(url) {
+    console.log(1);
+    return new Overlay(url);
 }
